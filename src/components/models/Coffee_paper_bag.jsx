2bd7de9619eb9963e67d8887a67178ae872c5635
Candice Ariel <candicewangsa@gmail.com>
2026-04-16 18:54:51 +0700

    position model

--- src/components/models/Coffee_paper_bag.jsx
+++ src/components/models/Coffee_paper_bag.jsx
@@ -13,7 +13,7 @@ import { Center, useGLTF } from '@react-three/drei'
 export function CoffeePaperBagModel(props) {
   const { nodes, materials } = useGLTF('/models/coffee_paper_bag.glb')
   return (
-    <group {...props} dispose={null} scale={0.4}>
+    <group {...props} dispose={null} scale={0.1}>
         <Center>
           <mesh geometry={nodes.Object_2.geometry} material={materials.set1} rotation={[-Math.PI / 2, 0, 0]} />
         </Center>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -7,15 +7,15 @@ import { CoffeePaperBagModel } from '../components/models/Coffee_paper_bag';
 
 const Hero = () => {
   return (
-    <section id="hero" className="flex-col justify-center h-screen w-full">
+    <section id="hero" className="flex-row ">
       <div>
         <p>LEFT</p>
       </div>
       {/* RIGHT : 3D MODEL */}
-      <div>
+      <div className="h-full">
           <Canvas
             shadows
-            camera={{ position: [5, 5, 7], fov: 20 }}
+            camera={{ position: [5, 0, 10], fov: 20 }}
             className="w-full" 
           >
             <ambientLight intensity={0.4} />
@@ -26,7 +26,7 @@ const Hero = () => {
               enablePan={false} 
               // CRITICAL: This centers the bag in the camera view 
               // adjust the '2' up or down to move the bag's position in the frame
-              target={[0, 1, 0]} 
+              target={[-2, 0 , 1]} 
               minPolarAngle={Math.PI / 5} 
               maxPolarAngle={Math.PI / 2} 
             />
